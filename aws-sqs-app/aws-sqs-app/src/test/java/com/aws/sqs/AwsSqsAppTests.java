402acc6489f58f76a037b6170473cc554acaedf0
Umakant Mishra <62796358+umakant2407@users.noreply.github.com>
2022-02-07 22:50:11 +0530

    Add files via upload

---
+++ aws-sqs-app/aws-sqs-app/src/test/java/com/aws/sqs/AwsSqsAppTests.java
@@ -0,0 +1,13 @@
+package com.aws.sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSqsAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
